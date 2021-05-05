0a8b1480e0b8aa567977d2d982a598feedfd134b
MtHonorio <matheushonorio@eng.ci.ufpb.br>
2021-05-05 14:01:15 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/mthonorio" target="_blank" rel="noreferrer">Matheus B. Honorio</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/mt.honorio" target="_blank" rel="noreferrer">@mt.honorio</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
